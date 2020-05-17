e8b1495101b7a16bb4328c6ef3b2ae7669148919
wagnersantos <wagtvc@gmail.com>
2020-05-17 18:12:12 -0300

    added migrations

---
+++ src/shared/infra/typeorm/migrations/1589749395230-CreateOrders.ts
@@ -0,0 +1,55 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableForeignKey,
+  Table,
+} from 'typeorm';
+
+export default class CreateOrders1589749395230 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'customer_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        name: 'customerKey',
+        columnNames: ['customer_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'customers',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders', 'customerKey');
+    await queryRunner.dropTable('orders');
+  }
+}
